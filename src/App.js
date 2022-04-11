23d9dfc0824a019d18b9872ce7eff294c3597029
kckoh <kyungcheolkoh@gmail.com>
2022-04-10 17:22:04 -0700

    very simple mvp

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import './App.css'
 import 'survey-react/modern.min.css';
 import { Survey, StylesManager, Model } from 'survey-react';
 
-var gResults;
+
 function GoodCalculator(props){
   let results = 0
   for (var prop in props) {
     if (Object.prototype.hasOwnProperty.call(props, prop)) {
       console.log(typeof props[prop] )
-        if ( typeof props[prop] === "boolean"){
+        if ( props[prop] == true){
+          if(prop === "peace"){
+              continue;
+          }
           results += 10;
+          console.log(prop, results)
           continue;
         }
+
         results += props[prop]
-        console.log (props[prop])
+        console.log(prop, results)
+        
     }
     
 }
   console.log(results)
-  gResults = results;
   return (results)
   
   
@@ -45,78 +50,78 @@ var surveyJson = {
     }
 ]},
 
-// {
-//   elements: [
-//   {
-//       "type": "boolean",
-//       "name": "relationshipLoved",
-//       "title": "Relationshiop",
-//       "label": "Do you have a person you love?",
-//       "isRequired": true
-//   },
-//   {
-//     "type": "boolean",
-//     "name": "relationshipFriend",
-//     "title": "Relationshiop",
-//     "label": "Do you have a friend you can rely on?",
-//     "isRequired": true
-// }
-// ]},
+{
+  elements: [
+  {
+      "type": "boolean",
+      "name": "relationshipLoved",
+      "title": "Relationshiop",
+      "label": "Do you have a person you love?",
+      "isRequired": true
+  },
+  {
+    "type": "boolean",
+    "name": "relationshipFriend",
+    "title": "Relationshiop",
+    "label": "Do you have a friend you can rely on?",
+    "isRequired": true
+}
+]},
 
-// {
-//   elements: [
-//   {
-//       "type": "boolean",
-//       "name": "wealth",
-//       "title": "Wealth",
-//       "label": "Can we find happiness in wealth?",
-//       "isRequired": true
-//   },
+{
+  elements: [
+  {
+      "type": "boolean",
+      "name": "wealth",
+      "title": "Wealth",
+      "label": "Can we find happiness in wealth?",
+      "isRequired": true
+  },
   
-// ]},
-// {
-//   elements: [
-//   {
-//       "type": "boolean",
-//       "name": "ability",
-//       "title": "Ability",
-//       "label": "Do you have the ability to change the world?",
-//       "isRequired": true
-//   },
+]},
+{
+  elements: [
+  {
+      "type": "boolean",
+      "name": "ability",
+      "title": "Ability",
+      "label": "Do you have the ability to change the world?",
+      "isRequired": true
+  },
   
-// ]},
-// {
-//   elements: [
-//   {
-//       "type": "boolean",
-//       "name": "hospitality",
-//       "title": "Hospitality",
-//       "label": "Are you friendly towards strangers?",
-//       "isRequired": true
-//   },
+]},
+{
+  elements: [
+  {
+      "type": "boolean",
+      "name": "hospitality",
+      "title": "Hospitality",
+      "label": "Are you friendly towards strangers?",
+      "isRequired": true
+  },
   
-// ]},{
-//   elements: [
-//   {
-//       "type": "boolean",
-//       "name": "peace",
-//       "title": "Peace",
-//       "label": "Do you have a trauma?",
-//       "isRequired": true
-//   },
+]},{
+  elements: [
+  {
+      "type": "boolean",
+      "name": "peace",
+      "title": "Peace",
+      "label": "Do you have a trauma?",
+      "isRequired": true
+  },
   
-// ]},
-// {
-//   elements: [
-//   {
-//       "type": "boolean",
-//       "name": "ability",
-//       "title": "Ability",
-//       "label": "Do you have a power to control someone?",
-//       "isRequired": true
-//   },
+]},
+{
+  elements: [
+  {
+      "type": "boolean",
+      "name": "ability",
+      "title": "Ability",
+      "label": "Do you have a power to control someone?",
+      "isRequired": true
+  },
   
-// ]},
+]},
 {
   elements: [
     {
@@ -129,63 +134,33 @@ var surveyJson = {
   
 ]},
 
-// {
-//   elements: [
-//     {
-//       name: "Freedom",
-//       title: "On a scale of zero to ten, how likely do you feel you are freed?",
-//       type: "rating",
-//       rateMin: 0,
-//       rateMax: 10,
-//     }
+{
+  elements: [
+    {
+      name: "Freedom",
+      title: "On a scale of zero to ten, how likely do you feel you are freed?",
+      type: "rating",
+      rateMin: 0,
+      rateMax: 10,
+    }
   
-// ]},
+]},
 
 
-  //  {
-  //   elements: [{
-  //     name: "satisfaction-score",
-  //     title: "How would you describe your life as it is?",
-  //     type: "radiogroup",
-  //     choices: [
-  //       { value: 5, text: "Fully satisfying" },
-  //       { value: 4, text: "Generally satisfying" },
-  //       { value: 3, text: "Neutral" },
-  //       { value: 2, text: "Rather unsatisfying" },
-  //       { value: 1, text: "Not satisfying at all" }
-  //     ],
-  //     isRequired: true
-  //   },
-  // ]
-  // }, 
 
-  // {
-  //   elements: [
 
-    
-  //    {
-  //     name: "nps-score",
-  //     title: "On a scale of zero to ten, how likely are you to be happy?",
-  //     type: "rating",
-  //     rateMin: 0,
-  //     rateMax: 10,
-  //   }],
-  //   visibleIf: "{satisfaction-score} >= 4"
-  // }, {
-  //   elements: [{
-  //     name: "how-can-we-improve",
-  //     title: "In your opinion, how could we improve our product?",
-  //     type: "comment"
-  //   }],
-  //   visibleIf: "{satisfaction-score} = 3"
-  // }, {
-  //   elements: [{
-  //     name: "disappointing-experience",
-  //     title: "Please let us know why you had such a disappointing experience with our product",
-  //     type: "comment"
-  //   }],
-  //   visibleIf: "{satisfaction-score} =< 2"
-  // }
+    {
+      elements: [
+        {
+          name: "satisfactionScore",
+      title: "On a scale of zero to ten, how happy are you with your life?",
+      type: "rating",
+      rateMin: 0,
+      rateMax: 10,
+        }
+      
+    ]},
+  
 
 
 ],
